656f8a24eff210476215988ee318164c6a1e3ec3
seoyoungKim <seoyoungkim003@gmail.com>
2024-10-01 18:00:52 +0900

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,15 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            {CORE_CONCEPTS.map((conceptItem) => 
+              (<CoreConcept key={ conceptItem.title } {...conceptItem}/>
+
+              ))}
+            {/* <CoreConcept {...CORE_CONCEPTS[0]}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
-            
+             */}
 
           </ul>
         </section>
